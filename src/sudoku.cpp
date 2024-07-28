31db94b9d6b6df427439999f4a210c968a4f77af
Ishwor Basyal <ishwor8basyal@gmail.com>
2024-07-28 19:01:02 +0545

    Numbers can be added

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <array>
 
 using std::array, std::cout, std::endl;
 
@@ -17,19 +18,20 @@ class board
 {
 private:
     int2d screen;
-    int row, column, unitSize,hoveringCell;
+    int row, column, unitSize;
     Vector2 plotStart, plotEnd, hLineStart, hLineEnd, vLineStart, vLineEnd;
-    bool hover;
-    inputHandling* input;
+    bool hover,cellSelected;
+    inputHandling *input;
+    int hoveringIndex;
+    array<char, 9 * 9> cellValues;
 
-public:    
-    board();    
+public:
+    board();
     ~board();
 
-    void drawBoard();       // Draws the necessary graphics each frame
-    void setup();           // Setup the required raylib window
+    void drawBoard(); // Draws the necessary graphics each frame
+    void setup();     // Setup the required raylib window
     int hoverCheck();
-
 };
 
 class inputHandling
@@ -39,26 +41,29 @@ private:
     int2d mouseCursorPosition;
     bool mouseButtonPressed;
     int currentCell;
+    bool firstClick;
+
 public:
     inputHandling();
     ~inputHandling();
-    
+
     int getCurrentKeyPressed();
     int2d getMouseCursorPosition();
-    bool getMouseButtonPressed();
+    bool getMouseButtonPressed();  
+    bool getFirstClick();
+    bool setFirstClick();  
 
     // int cellHover();            // Checks if mouse is within a cell and returns the cell number;
-
-
 };
-
 
 board::board()
 {
-    input=new inputHandling();
-    row = 9, column = 9, unitSize = 50,hoveringCell=-1;
+    input = new inputHandling();
+    row = 9, column = 9, unitSize = 50; //,hoveringCell = -1;
     screen = {900, 900};
-    hover=false;
+    hover = false;
+    cellSelected=false;
+    hoveringIndex=-1;
 
     // x,y coordinates of the leftmost and topmost point from where the graphics starts
     plotStart = {float(screen.x) / 2 - column * unitSize / 2, float(screen.y) / 2 - row * unitSize / 2};
@@ -71,38 +76,69 @@ board::board()
 
     vLineStart = plotStart;
     vLineEnd = {plotStart.x, plotEnd.y};
+
+    // initialize the board
+    for(auto &value:cellValues)    
+        value=0;
+    
 }
 
 void board::drawBoard()
 {
-    int pressed=input->getCurrentKeyPressed();
-    if(pressed) cout<<pressed<<endl;
-    
-    Color affectedCells={226, 235, 243,255};
-    Color affectedCellMain={68, 114, 202,128};
-    Color selectedCell={187, 222, 251,255};
-    
+    char enteredNumber = input->getCurrentKeyPressed();
 
-    int hoveringIndex=hoverCheck();
-    
-    if(hover)
+    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT))    
+    {        
+        if(input->getFirstClick())
+            cellSelected=false;            
+        
+        else
+        {
+            cellSelected=true;
+        }
+        
+        input->setFirstClick();        
+    }               
+
+    Color affectedCells = {226, 235, 243, 255};
+    Color affectedCellMain = {68, 114, 202, 128};
+    Color selectedCell = {187, 222, 251, 255};
+
+    Rectangle highlightingEffect;
+
+    if(cellSelected)
     {
-        Rectangle highlightingEffect;
+        if(enteredNumber)
+            cellValues.at(hoveringIndex)=enteredNumber;
+        cout<<cellValues.at(hoveringIndex)<<endl;
+    }        
+    else
+        hoveringIndex=hoverCheck();
+    
 
+    if (hover)
+    {
         // Highlighting affected cells
-        highlightingEffect={plotStart.x,plotStart.y+(hoveringIndex/9)*unitSize,unitSize*1.0f*column,unitSize*1.0f};
-        DrawRectangleRec(highlightingEffect,affectedCells);
+        highlightingEffect = {plotStart.x, plotStart.y + (hoveringIndex / 9) * unitSize, unitSize * 1.0f * column, unitSize * 1.0f};
+        DrawRectangleRec(highlightingEffect, affectedCells);
 
-        highlightingEffect={plotStart.x+(hoveringIndex%9)*unitSize,plotStart.y,unitSize*1.0f,unitSize*1.0f*row};
-        DrawRectangleRec(highlightingEffect,affectedCells);
+        highlightingEffect = {plotStart.x + (hoveringIndex % 9) * unitSize, plotStart.y, unitSize * 1.0f, unitSize * 1.0f * row};
+        DrawRectangleRec(highlightingEffect, affectedCells);
 
-        highlightingEffect={plotStart.x+((hoveringIndex%9)/3)*unitSize*3,plotStart.y+(hoveringIndex/27)*unitSize*3,unitSize*column/3.0f,unitSize*row/3.0f};
-        DrawRectangleRec(highlightingEffect,affectedCells);
+        highlightingEffect = {plotStart.x + ((hoveringIndex % 9) / 3) * unitSize * 3, plotStart.y + (hoveringIndex / 27) * unitSize * 3, unitSize * column / 3.0f, unitSize * row / 3.0f};
+        DrawRectangleRec(highlightingEffect, affectedCells);
 
-        // Drawing the hovering cell highlight    
-        highlightingEffect={plotStart.x+(hoveringIndex%9)*unitSize,plotStart.y+(hoveringIndex/9)*unitSize,unitSize*1.0f-1,unitSize*1.0f-1};
-        DrawRectangleRec(highlightingEffect,WHITE); 
-        DrawRectangleRec(highlightingEffect,affectedCellMain);
+        // Drawing the hovering cell highlight
+        highlightingEffect = {plotStart.x + (hoveringIndex % 9) * unitSize, plotStart.y + (hoveringIndex / 9) * unitSize, unitSize * 1.0f - 1, unitSize * 1.0f - 1};
+        DrawRectangleRec(highlightingEffect, WHITE);
+        DrawRectangleRec(highlightingEffect, affectedCellMain);
+
+        if (cellSelected)
+        {
+            DrawRectangleRec(highlightingEffect, WHITE);
+            DrawRectangleRec(highlightingEffect, selectedCell);
+        }
+        
     }
 
     // Drawing the 9 cells
@@ -124,14 +160,16 @@ void board::drawBoard()
 
         vLineStart.x += unitSize;
         vLineEnd.x += unitSize;
-    }    
-    // Displaying the numbers
-    {
-        char text[2]={' ','\0'};
-        Rectangle highlightingEffect={plotStart.x+(hoveringIndex%9)*unitSize,plotStart.y+(hoveringIndex/9)*unitSize,unitSize*1.0f-1,unitSize*1.0f-1};
-        DrawText(text,highlightingEffect.x+unitSize/2,highlightingEffect.y+unitSize/2,10,RED);
     }
+    // Displaying the numbers
+    for(int i=0;i<row*column;i++)
+    {
+        char text[2]={cellValues.at(i),'\0'};       
 
+        Rectangle highlightingEffect = {plotStart.x + (i % 9) * unitSize, plotStart.y + (i / 9) * unitSize, unitSize * 1.0f - 1, unitSize * 1.0f - 1};
+        DrawText(text, highlightingEffect.x + unitSize / 2, highlightingEffect.y + unitSize / 2, 20, RED);
+    }
+    
     plotStart = {float(screen.x) / 2 - column * unitSize / 2, float(screen.y) / 2 - row * unitSize / 2};
     plotEnd = {float(screen.x) / 2 + column * unitSize / 2, float(screen.y) / 2 + row * unitSize / 2};
 
@@ -159,27 +197,27 @@ int board::hoverCheck()
 {
     // Checking if hovering
     int2d mouse = input->getMouseCursorPosition();
-    int hoveringCellIndex=-1;
-    if((mouse.x>=plotStart.x && mouse.x<=plotEnd.x)&&(mouse.y>=plotStart.y && mouse.y<=plotEnd.y))
+    int hoveringCellIndex = -1;
+    if ((mouse.x >= plotStart.x && mouse.x <= plotEnd.x) && (mouse.y >= plotStart.y && mouse.y <= plotEnd.y))
     {
-        hover=true;
-        hoveringCellIndex=int(mouse.x-plotStart.x)/unitSize+int(mouse.y-plotStart.y)/unitSize*column;
+        hover = true;
+        hoveringCellIndex = int(mouse.x - plotStart.x) / unitSize + int(mouse.y - plotStart.y) / unitSize * column;
     }
     else
     {
         hover = false;
-        hoveringCellIndex=-1;
-    }    
-    return hoveringCellIndex;    
+        hoveringCellIndex = -1;
+    }
+    return hoveringCellIndex;
 }
-
 
 inputHandling::inputHandling()
 {
-    currentKeyPress=GetKeyPressed();
-    mouseCursorPosition.x=GetMouseX();
-    mouseCursorPosition.y=GetMouseY();
-    mouseButtonPressed=IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+    currentKeyPress = GetKeyPressed();
+    mouseCursorPosition.x = GetMouseX();
+    mouseCursorPosition.y = GetMouseY();
+    mouseButtonPressed = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+    firstClick=false;
     // mouseButtonPressed=!IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
 }
 
@@ -189,26 +227,35 @@ inputHandling::~inputHandling()
 }
 
 int inputHandling::getCurrentKeyPressed()
-{   
+{
     currentKeyPress = GetKeyPressed();
     return currentKeyPress;
 }
 
 int2d inputHandling::getMouseCursorPosition()
 {
-    mouseCursorPosition.x=GetMouseX();
-    mouseCursorPosition.y=GetMouseY();
+    mouseCursorPosition.x = GetMouseX();
+    mouseCursorPosition.y = GetMouseY();
 
     return mouseCursorPosition;
 }
 
 bool inputHandling::getMouseButtonPressed()
 {
-    mouseButtonPressed=IsMouseButtonDown(MOUSE_BUTTON_LEFT);
+    mouseButtonPressed = IsMouseButtonDown(MOUSE_BUTTON_LEFT);
     return mouseButtonPressed;
 }
 
+bool inputHandling::getFirstClick()
+{
+    return firstClick;
+}
 
+bool inputHandling::setFirstClick()
+{
+    firstClick=!firstClick;
+    return firstClick;
+}
 
 int main()
 {
@@ -219,10 +266,9 @@ int main()
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(WHITE); 
-        // inp.getCurrentKeyPressed();       
+        ClearBackground(WHITE);
+        // inp.getCurrentKeyPressed();
         sudoku.drawBoard();
-        
 
         EndDrawing();
     }
